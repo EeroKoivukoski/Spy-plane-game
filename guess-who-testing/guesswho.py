c3512f18aa14d125f1c89553eb88e876f540beab
zakke-username <175124121+zakke-username@users.noreply.github.com>
2024-09-18 22:50:05 +0300

    guess who prototyping

--- guess-who-testing/guesswho.py
+++ guess-who-testing/guesswho.py
@@ -1,45 +1,71 @@
 # Tuntomerkki/pidätys -systeemin pohjaa kokeilun vuoksi
-# Tästä rakennetaan ja muokataan pääpeliin
-
+# Tästä voi rakentaa ja muokata ja lisätä pääpeliin
 
 import random
 
 
 def main():
-    enemy = generate_person_description()
-    print(f"THIEF: {enemy}\n")
+    enemy = generate_person()
+    print(f"THIEF: {describe_person(enemy)}\n")
 
     print("You see the following people...\nWho is the spy?\n")
     enemy_index = random.randint(0,9)
     for i in range(10):
         if i == enemy_index:
-            print(f"{[i+1]} {enemy}")
-            continue
-        npc = generate_person_description()
-        while npc == enemy:
-            npc = generate_person_description()
-        print(f"{[i+1]} {npc}")
+            print(f"{[i+1]} {describe_person(enemy)}")
+        else:
+            npc = generate_person()
+            while npc == enemy:
+                npc = generate_person()
+            print(f"[{i+1}] {describe_person(npc)}")
 
-    selection = int(input("\nEnter a number to arrest a person: "))
-    if selection == enemy_index + 1:
+    selection = int(input("\nEnter a number to arrest a person: ")) - 1
+    if selection == enemy_index:
         print("You arrested the thief! You win!")
     else:
         print("You arrested an innocent person. You lose.")
 
 
-def generate_person_description():
-    person = {
-        "height": ["short", "average height", "tall"],
-        "age": ["young", "adult", "middle-aged", "old"],
-        "gender": ["man", "woman"],
-        "hair": ["blonde", "brown", "dark", "gray", "no"],
-        "clothes": ["a hawaiian shirt", "a suit", "a dress", "a hoodie", "a baseball cap", "sunglasses"]
+def generate_person():
+    person = {}
+    features = {
+        "height":
+            ["Short",
+             "Average height",
+             "Tall"],
+        "age":
+            ["young",
+             "adult",
+             "middle-aged",
+             "old"],
+        "gender":
+            ["man",
+             "woman"],
+        "head":
+            ["blonde hair",
+             "brown hair",
+             "long hair",
+             "no hair",
+             "a baseball cap",
+             "sunglasses",
+             "headphones"],
+        "clothes":
+            ["a hawaiian shirt",
+             "a suit",
+             "a dress",
+             "a hoodie",
+             "a denim jacket",
+             "sweatpants"]
     }
 
-    for feature in person:
-        person[feature] = random.choice(person[feature])
+    for feature in features:
+        person[feature] = random.choice(features[feature])
 
-    return f"{person['height'].capitalize()} {person['age']} {person['gender']} with {person['hair']} hair and {person['clothes']}"
+    return person
+
+
+def describe_person(p):
+    return f"{p['height']} {p['age']} {p['gender']} with {p['head']} and {p['clothes']}"
 
 
 main()
