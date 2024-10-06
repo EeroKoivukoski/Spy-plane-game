0fa35a96e096199225afa2f72d3427004c9ca373
EeroKoivukoski <eero.koivukoski@metropolia.fi>
2024-10-06 13:45:09 +0300

    tiedostojen uudelleennimeäminen

--- Frontend/game.py
+++ Frontend/game.py
@@ -2,11 +2,9 @@ import random
 import mysql.connector
 from geopy import distance
 import tietokantatunnukset
-import Usualsuspects
+import minipeli
 import asci_lib
-from Frontend.Usualsuspects import numerochecker
-
-# asenna python packaget geopy, mysql-connector-python 8.0.29
+#asenna python packaget geopy, mysql-connector-python 8.0.29
 
 connection = mysql.connector.connect(
     host="127.0.0.1",
@@ -14,112 +12,66 @@ connection = mysql.connector.connect(
     database="flight_game",
     user=tietokantatunnukset.user,
     password=tietokantatunnukset.password,
-    autocommit=True)
+    autocommit=True
+)
+
+player = {}
+enemy = {}
 
 
 def main():
-    #aloitus arvot
-    continue_check=0
+    #Intro ja login
+    asci_lib.asci("intro")
+    input("Enter your name to start the game")
+
     all_airports = get_airports()
+    #Rosvon ulkonäkö
     suspect = generate_person()
+
+    # Pelaaja ja rosvo random kentillä (todo: varmista että rosvo tarpeeksi kaukana pelaajasta)
     current = random.choice(all_airports)
     last = 0
     enemy_airport = random.choice(all_airports)
 
-    asci_lib.asci("intro")
-    username = input("Enter your name to start the game: ")
+    #Traits
+    madness = 0
+    foodpoisoning = 0
+    gun = 0
+    guns = 0
 
-    player = {
-        "name": username,
-        "currentname":current["name"] ,
-        "currentcountry": current["country"],
-        "currentlatitude": current["latitude"],
-        "currentlongitude": current["longitude"],
-        "suspectheight": suspect["height"],
-        "suspectage": suspect["age"],
-        "suspectgender": suspect["gender"],
-        "suspectclothes": suspect["clothes"],
-        "suspecthead": suspect["head"],
-        "enemy_airport_name": enemy_airport["name"],
-        "enemy_airport_country": enemy_airport["country"],
-        "enemy_airport_latitude": enemy_airport["latitude"],
-        "enemy_airport_longitude": enemy_airport["longitude"],
-        "madness": 0,
-        "foodpoisoning": 0,
-        "gun": 0,
-        "guns": 0,
-        "km_flown": 0,
-        "day": 0,
-        "last_move_day": 0,
-        "given_clues": "Given@Clues",
-    }
+    # Muuttujat
+    km_flown = 0
+    day = 0
+    last_move_day = 0
+    # clues = 0
+    given_clues = []
+    max_days = 30
 
-    data = get_player_data(username)
-
-    if data:
-        sel = input("[1]Uusi peli \n[2]Jatka vanhaa")
-        sel = numerochecker(sel, 2)
-        if sel == 2:
-            player = data
-            continue_check = 1
-
-        # Valitaan uusi peli = päivitetään rivi tietokannan taulussa
-        else:
-            update_player(player)
-            continue_check = 2
-
-    # Dataa ei löytynyt -> lisätään rivi tauluun
+    #Tutorial
+    asci_lib.asci('tutorial')
+    tutorial=input('')
+    tutorial=minipeli.numerochecker(tutorial,2)
+    if tutorial == 1:
+        print("you selected option 1")
     else:
-        insert_new_player(player)
-
-    suspect = {"height": player["suspectheight"], "age": player["suspectage"],"gender":player["suspectgender"], "clothes":player["suspectclothes"], "head":player["suspecthead"]}
-    current = {"name": player["currentname"], "country": player["currentcountry"],"latitude": player["currentlatitude"],"longitude": player["currentlongitude"]}
-    enemy_airport = {"name": player["enemy_airport_name"], "country": player["enemy_airport_country"], "latitude": player["enemy_airport_latitude"], "longitude": player['enemy_airport_longitude']}
-    madness = player["madness"]
-    foodpoisoning = player["foodpoisoning"]
-    gun = player["gun"]
-    guns = player["guns"]
-    km_flown = player["km_flown"]
-    day = player["day"]
-    last_move_day = player["last_move_day"]
-    given_clues=player["given_clues"]
-    given_clues = list(given_clues.replace("@",",").split(","))
+        print("you selected option 2")
+    input("press any key to continue")
+    asci_lib.asci("loreone")
+    mission=input()
+    mission=minipeli.numerochecker(mission,2)
+    if mission == 2:
+        asci_lib.asci('missionnah')
+        exit()
 
 
-
-    # Tutorial
-    if continue_check == 0:
-        asci_lib.asci('tutorial')
-        tutorial = input('')
-        tutorial = Usualsuspects.numerochecker(tutorial, 2)
-        if tutorial == 1:
-            print("you selected option 1")
-        else:
-            print("you selected option 2")
-        input("press any key to continue")
-
-    if continue_check == 2 or continue_check == 0:
-        asci_lib.asci("loreone")
-        mission = input()
-        mission = Usualsuspects.numerochecker(mission, 2)
-        if mission == 2:
-            asci_lib.asci('missionnah')
-            exit()
-
-    if len(given_clues) > 2:
-        x=len(given_clues)-2
-        while x != 0:
-            print("You know the spies following fetures: ",suspect[given_clues[1+x]])
-            x-=1
     # Main loop
     while True:
         if day < 0:
             day = 0
         # Lopeta peli jos päivät täynnä
-        if 30 < day:
+        if max_days <= day:
             asci_lib.asci("gameover")
             exit()
-
 
         # Tulosta info
         if last == 0:
@@ -127,14 +79,14 @@ def main():
             last = current
         elif last['country'] == current['country'] and last['name'] != current['name']:
             print(f"\nWelcome to {current['name']}!")
-            last = current
+            last =current
         elif last != current:
             print(f"\nWelcome to {current['country']}! You are currently at {current['name']}.\n")
             last = current
         else:
             print(f"\nYou are still in {current['country']} at {current['name']}.\n")
         print(f"You have travelled {km_flown} km in {day} day(s).\n")
-        print(f"You have {30 - day} days left.")
+        print(f"You have {max_days - day} days left.")
 
         # Siirrä rosvoa
         if day - last_move_day >= 5:
@@ -146,17 +98,15 @@ def main():
         # Vaihtoehdot voisi olla esim: [1] Fly to another airport [2] Stay and look for clues
         # todo: jos samalla kentällä kuin rosvo -> suoraan guess who-arvailuun, ei muita vaihtoehtoja (miksi?)
         if current == enemy_airport:
-            eveningoptions = input(
-                '\n[1] Fly to another airport \n[2] Look around \n[3] try to guess who the spy is at this airport \nWhat do you want to do: ')
-            eveningoptions = Usualsuspects.numerochecker(eveningoptions, 3)
+            eveningoptions = input('\n[1] Fly to another airport \n[2] Look around \n[3] try to guess who the spy is at this airport \nWhat do you want to do: ')
+            eveningoptions = minipeli.numerochecker(eveningoptions, 3)
         else:
-            eveningoptions = input(
-                '\n[1] Fly to another airport \n[2] Stay at this airport  \nWhat do you want to do: ')
-            eveningoptions = Usualsuspects.numerochecker(eveningoptions, 2)
+            eveningoptions = input('\n[1] Fly to another airport \n[2] Stay at this airport  \nWhat do you want to do: ')
+            eveningoptions = minipeli.numerochecker(eveningoptions, 2)
 
         # Lennä muualle
         if eveningoptions == 1:
-            day += 1
+            day+=1
             print("Where would you like to fly next?")
             print(f"You are currently at {current['name']} in {current['country']}.")
             print(f"{navigation(current, enemy_airport)}\n")
@@ -187,7 +137,7 @@ def main():
                 except IndexError:
                     print("That's not a valid number!")
                 else:
-                    selection = int(selection) - 1
+                    selection = int(selection)-1
                     if selection < 0:
                         print("That's not a valid number!")
                     else:
@@ -199,7 +149,7 @@ def main():
         # Koodi toistuu
         # todo: minipelin tulos toiseen funktioon (rakennetta uusiksi?)
         elif eveningoptions == 2:
-            minipelitulos = Usualsuspects.minipeli(current['country'], madness, foodpoisoning, gun, guns)
+            minipelitulos = minipeli.minipeli(current['country'],madness,foodpoisoning,gun,guns)
             if minipelitulos == 2:
                 print_clue(suspect, given_clues)
             elif minipelitulos == 3:
@@ -224,7 +174,7 @@ def main():
             elif minipelitulos == 8:
                 print("You got a gun... even though it seems to be an antique.")
                 gun = 1
-                guns = 1
+                guns=1
                 asci_lib.asci("GUN")
             elif minipelitulos == 9:
                 print("You lost a day but got a clue!")
@@ -260,53 +210,11 @@ def main():
             if selection == enemy_index:
                 print("You arrested the thief!")
                 asci_lib.asci("vakooja")
-                print("You win! \nYou won in", day - 1, "Days!")
+                print("You win! \nYou won in",day-1,"Days!")
                 exit()
             else:
                 print("You arrested an innocent person. The spy hears about it and moves to another airport.")
                 enemy_airport = random.choice(all_airports)
-
-        given_clues="@".join(given_clues)
-        player = {
-            "name": username,
-            "currentname":current["name"] ,
-            "currentcountry": current["country"],
-            "currentlatitude": current["latitude"],
-            "currentlongitude": current["longitude"],
-            "suspectheight": suspect["height"],
-            "suspectage": suspect["age"],
-            "suspectgender": suspect["gender"],
-            "suspectclothes": suspect["clothes"],
-            "suspecthead": suspect["head"],
-            "enemy_airport_name": enemy_airport["name"],
-            "enemy_airport_country": enemy_airport["country"],
-            "enemy_airport_latitude": enemy_airport["latitude"],
-            "enemy_airport_longitude": enemy_airport["longitude"],
-            "madness": madness,
-            "foodpoisoning": foodpoisoning,
-            "gun": gun,
-            "guns": guns,
-            "km_flown": km_flown,
-            "day": day,
-            "last_move_day": last_move_day,
-            "given_clues": given_clues,
-        }
-        update_player(player)
-        suspect = {"height": player["suspectheight"], "age": player["suspectage"], "gender": player["suspectgender"],
-                   "clothes": player["suspectclothes"], "head": player["suspecthead"]}
-        current = {"name": player["currentname"], "country": player["currentcountry"],
-                   "latitude": player["currentlatitude"], "longitude": player["currentlongitude"]}
-        enemy_airport = {"name": player["enemy_airport_name"], "country": player["enemy_airport_country"],
-                         "latitude": player["enemy_airport_latitude"], "longitude": player['enemy_airport_longitude']}
-        madness = player["madness"]
-        foodpoisoning = player["foodpoisoning"]
-        gun = player["gun"]
-        guns = player["guns"]
-        km_flown = player["km_flown"]
-        day = player["day"]
-        last_move_day = player["last_move_day"]
-        given_clues = player["given_clues"]
-        given_clues = list(given_clues.split("@"))
 
 
 def get_closest_airports(current, count):
@@ -317,14 +225,14 @@ def get_closest_airports(current, count):
     airports.sort(key=lambda d: calculate_distance(current, d))
 
     # Palauta listasta indeksit 1 -> count+1 (0 = nykyinen kenttä)
-    return airports[1:count + 1]
+    return airports[1:count+1]
 
 
 def calculate_distance(current, target):
     # Laskee etäisyyden koordinaattien välillä geopy-kirjaston avulla
     a = (current["latitude"], current["longitude"])
     b = (target["latitude"], target["longitude"])
-    dist = round(distance.distance(a, b).km)
+    dist = round(distance.distance(a,b).km)
     return dist
 
 
@@ -335,7 +243,7 @@ def get_airports_radius(current, radius_km):
     for i in airports:
         dist = calculate_distance(current, i)
         if dist <= radius_km and current["name"] != i["name"]:
-            out.append(i)
+           out.append(i)
     return out
 
 
@@ -432,7 +340,7 @@ def describe_person(p):
 
 def print_clue(suspect, given_clues):
     # Jos annetut vihjeet on tuntomerkkien määrä -> kaikki vihjeet annettu
-    if (len(given_clues))-2 == len(suspect.keys()):
+    if len(given_clues) == len(suspect.keys()):
         print("All clues given!")
         return
 
@@ -453,103 +361,6 @@ def print_clue(suspect, given_clues):
         print(f"The suspect has {suspect[feature]}.")
     elif feature == "clothes":
         print(f"The suspect is wearing {suspect[feature]}.")
-
-
-def get_player_data(username):
-    sql = f"""
-    select name, currentname,currentcountry,currentlatitude,currentlongitude, suspectheight,suspectage,suspectgender,suspectclothes,suspecthead,enemy_airport_name, enemy_airport_country, enemy_airport_latitude,enemy_airport_longitude, madness, foodpoisoning, gun, guns, km_flown, day, last_move_day, given_clues from test_game
-    where name = '{username}'
-    """
-    cursor = connection.cursor()
-    cursor.execute(sql)
-    result = cursor.fetchall()
-    data = {}
-    if result:
-        data = {
-            "name": result[0][0],
-            "currentname": result[0][1],
-            "currentcountry": result[0][2],
-            "currentlatitude": result[0][3],
-            "currentlongitude": result[0][4],
-            "suspectheight": result[0][5],
-            "suspectage": result[0][6],
-            "suspectgender": result[0][7],
-            "suspectclothes": result[0][8],
-            "suspecthead": result[0][9],
-            "enemy_airport_name": result[0][10],
-            "enemy_airport_country": result[0][11],
-            "enemy_airport_latitude": result[0][12],
-            "enemy_airport_longitude": result[0][13],
-            "madness": result[0][14],
-            "foodpoisoning": result[0][15],
-            "gun": result[0][16],
-            "guns": result[0][17],
-            "km_flown": result[0][18],
-            "day": result[0][19],
-            "last_move_day": result[0][20],
-            "given_clues": result[0][21],
-        }
-    return data
-
-
-def update_player(player):
-    sql = f"""
-    update test_game
-    set currentname = '{player["currentname"]}',
-    currentcountry = '{player["currentcountry"]}',
-    currentlatitude = '{player["currentlatitude"]}',
-    currentlongitude ='{player["currentlongitude"]}',
-    suspectheight = '{player["suspectheight"]}',
-    suspectage = '{player["suspectage"]}',
-    suspectgender = '{player["suspectgender"]}',
-    suspectclothes = '{player["suspectclothes"]}',
-    suspecthead = '{player["suspecthead"]}',
-    enemy_airport_name = '{player["enemy_airport_name"]}',
-    enemy_airport_country = '{player["enemy_airport_country"]}',
-    enemy_airport_latitude = '{player["enemy_airport_latitude"]}',
-    enemy_airport_longitude = '{player["enemy_airport_longitude"]}',
-    madness = '{player["madness"]}',
-    foodpoisoning = '{player["foodpoisoning"]}',
-    gun = '{player["gun"]}',
-    guns = '{player["guns"]}',
-    km_flown = '{player["km_flown"]}',
-    day = '{player["day"]}',
-    last_move_day = '{player["last_move_day"]}',
-    given_clues = '{player["given_clues"]}'
-    where name = '{player["name"]}'
-    """
-    cursor = connection.cursor()
-    cursor.execute(sql)
-
-
-def insert_new_player(player):
-    sql = f"""
-    insert into test_game values
-    ('{player["name"]}',
-    '{player["currentname"]}',
-    '{player["currentcountry"]}',
-    '{player["currentlatitude"]}',
-    '{player["currentlongitude"]}',
-    '{player["suspectheight"]}',
-    '{player["suspectage"]}',
-    '{player["suspectgender"]}',
-    '{player["suspectclothes"]}',
-    '{player["suspecthead"]}',
-    '{player["enemy_airport_name"]}',
-    '{player["enemy_airport_country"]}',
-    '{player["enemy_airport_latitude"]}',
-    '{player["enemy_airport_longitude"]}',
-    '{player["madness"]}',
-    '{player["foodpoisoning"]}',
-    '{player["gun"]}',
-    '{player["guns"]}',
-    '{player["km_flown"]}',
-    '{player["day"]}',
-    '{player["last_move_day"]}',
-    '{player["given_clues"]}')
-"""
-    cursor = connection.cursor()
-    cursor.execute(sql)
 
 
 main()
